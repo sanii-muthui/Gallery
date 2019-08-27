87e0aaf2df6820eb17202328f512364f71748e34
sanii-muthui <muthuisanii@gmail.com>
2019-08-27 10:15:53 +0300

    added httpREsponse

--- imageapp/views.py
+++ imageapp/views.py
@@ -28,7 +28,7 @@ def past_days_news(request, past_date):
         # Converts data from the string Url
         date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
     except ValueError:
-        # Raise 404 error when ValueError is thrown
+        # Raise 404 error when ValueError is thrown here
         raise Http404()
         assert False
 
